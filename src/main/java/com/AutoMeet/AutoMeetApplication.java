a72721ee68ba43b8a486a09364ef47adf40c309d
chanhee01 <joch4267@naver.com>
2024-04-11 19:39:38 +0900

    first commit

---
+++ src/main/java/com/AutoMeet/AutoMeetApplication.java
@@ -0,0 +1,13 @@
+package com.AutoMeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AutoMeet/AutoMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoMeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
