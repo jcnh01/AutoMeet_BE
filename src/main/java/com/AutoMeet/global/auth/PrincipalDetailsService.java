6cd31c0c4cf3df1645f42b99af821af6615c8cfe
chanhee01 <joch4267@naver.com>
2024-04-19 18:15:34 +0900

    feat(jwt) : principalDetails 추가

---
+++ src/main/java/com/AutoMeet/global/auth/PrincipalDetailsService.java
@@ -0,0 +1,22 @@
+package com.AutoMeet.global.auth;
+
+import com.AutoMeet.domain.user.model.User;
+import com.AutoMeet.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByEmail(email);
+        return new PrincipalDetails(userEntity);
+    }
+}
