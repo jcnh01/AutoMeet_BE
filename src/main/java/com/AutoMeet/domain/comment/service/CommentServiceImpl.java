867d763c3581bcbf322f098c93f370f308815457
chanhee01 <joch4267@naver.com>
2024-05-16 12:29:59 +0900

    feat : 자기가 참여한 회의가 아니면 댓글 작성 금지

--- src/main/java/com/AutoMeet/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/AutoMeet/domain/comment/service/CommentServiceImpl.java
@@ -2,6 +2,9 @@ package com.AutoMeet.domain.comment.service;
 
 import com.AutoMeet.domain.comment.Comment;
 import com.AutoMeet.domain.comment.repository.CommentRepository;
+import com.AutoMeet.domain.meet.exception.NotYourMeetingException;
+import com.AutoMeet.domain.meet.model.Meet;
+import com.AutoMeet.domain.meet.service.MeetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,10 +19,15 @@ import java.time.ZonedDateTime;
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
+    private final MeetService meetService;
 
     @Override
     @Transactional
     public void createComment(String meetingId, String content, Long userId) {
+
+        if (!IsMeetingUser(userId, meetingId)) {
+            throw new NotYourMeetingException(meetingId);
+        }
 
         ZoneId seoulZoneId = ZoneId.of("Asia/Seoul");
         ZonedDateTime seoulTime = ZonedDateTime.of(LocalDateTime.now(), seoulZoneId);
@@ -34,3 +42,10 @@ public class CommentServiceImpl implements CommentService {
         commentRepository.save(comment);
     }
 
+    public Boolean IsMeetingUser(Long userId, String meetingId) {
+        Meet meeting = meetService.findMeeting(meetingId);
+
+        return meeting.getUserIds().contains(userId);
+    }
+
+}
