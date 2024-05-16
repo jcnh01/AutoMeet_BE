f85f70fa19099eced33d13f3dc2a0c51c6f6f909
chanhee01 <joch4267@naver.com>
2024-05-16 12:30:49 +0900

    feat : ErrorCode 최신화

--- src/main/java/com/AutoMeet/global/exception/ErrorCode.java
+++ src/main/java/com/AutoMeet/global/exception/ErrorCode.java
@@ -9,7 +9,17 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // login
-    EMAIL_EXIST(HttpStatus.BAD_REQUEST, "Login01", "이메일이 이미 존재합니다.");
+    EMAIL_EXIST(HttpStatus.BAD_REQUEST, "Login01", "이메일이 이미 존재합니다."),
+
+    // auth
+    ACCESS_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "Auth01", "access 토큰이 만료되었습니다."),
+
+    // meeting
+    SESSION_NOT_EXIST(HttpStatus.BAD_REQUEST, "Meeting01", "세션이 존재하지 않습니다."),
+    MEETING_NOT_EXIST(HttpStatus.BAD_REQUEST, "Meeting02", "회의실이 존재하지 않습니다."),
+    RECORDING_NOT_EXIST(HttpStatus.BAD_REQUEST, "Meeting03", "녹화가 진행되지 않고 있습니다."),
+    NOT_YOUR_MEETING(HttpStatus.BAD_REQUEST, "Meeting04", "사용자가 참여한 회의가 아닙니다.");
+
 
     private final HttpStatus status;
     private final String code;
