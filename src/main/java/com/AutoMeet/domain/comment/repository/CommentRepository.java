1a625572cbd7cac0af85b77e154835005c49394b
chanhee01 <joch4267@naver.com>
2024-05-17 17:55:12 +0900

    delete : Mongo db 설계 수정으로 인해 삭제

--- src/main/java/com/AutoMeet/domain/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.AutoMeet.domain.comment.repository;
-
-import com.AutoMeet.domain.comment.Comment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CommentRepository extends MongoRepository<Comment, String> {
-}
