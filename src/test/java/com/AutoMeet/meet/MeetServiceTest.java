bda1828222569a5cab3e0c962e0e3cd0922e6d2c
chanhee01 <joch4267@naver.com>
2024-05-18 14:48:07 +0900

    test : 회의 보고서 수정 테스트

--- src/test/java/com/AutoMeet/meet/MeetServiceTest.java
+++ src/test/java/com/AutoMeet/meet/MeetServiceTest.java
@@ -1,5 +1,6 @@
 package com.AutoMeet.meet;
 
+import com.AutoMeet.domain.meet.dto.request.UpdateMeetRequest;
 import com.AutoMeet.domain.meet.exception.NotYourMeetingException;
 import com.AutoMeet.domain.meet.model.Meet;
 import com.AutoMeet.domain.meet.service.MeetService;
@@ -60,5 +61,51 @@ public class MeetServiceTest {
         // 회의에 참여한 사용자가 아니면 조회 불가능
         assertThrows(NotYourMeetingException.class, () ->
                 meetService.findOne(meetId, user3.getId()), "사용자가 참여한 회의가 아닙니다.");
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("회의 보고서 수정")
+    void updateMeet() {
+        UserRequestDto userDto = new UserRequestDto("email@naver.com", "password", "kim", 20);
+        userService.join(userDto);
+        User user = userService.findByEmail(userDto.getEmail());
+
+        List<Long> userIds = new ArrayList<>();
+        userIds.add(user.getId());
+
+        UserRequestDto userDto2 = new UserRequestDto("email2@naver.com", "password2", "lee", 30);
+        userService.join(userDto2);
+        User user2 = userService.findByEmail(userDto2.getEmail());
+
+        UserRequestDto userDto3 = new UserRequestDto("email3@naver.com", "password3", "park", 27);
+        userService.join(userDto3);
+        User user3 = userService.findByEmail(userDto3.getEmail());
+
+        userIds.add(user2.getId());
+
+        String meetId = meetService.save("title", "content", userIds);
+
+        UpdateMeetRequest request = new UpdateMeetRequest("new title", "new content");
+
+        meetService.updateMeeting(meetId, user.getId(), request);
+
+        Meet updateMeet = meetService.findMeeting(meetId);
+
+        // 수정 후 제목이 바뀌어져 있어야 함
+        Assertions.assertEquals(updateMeet.getTitle(), "new title");
+        Assertions.assertEquals(updateMeet.getContent(), request.getContent());
+
+        UpdateMeetRequest request2 = new UpdateMeetRequest("new title2", "new content2");
+
+        // 본인이 참여한 회의가 아니면 수정 불가능
+        assertThrows(NotYourMeetingException.class, () ->
+                meetService.updateMeeting(meetId, user3.getId(), request2), "사용자가 참여한 회의가 아닙니다.");
+
+        Meet finalMeet = meetService.findMeeting(meetId);
+
+        // new title2로 변경되어 있으면 안됨
+        Assertions.assertFalse(finalMeet.getTitle().equals("new title2"));
+        Assertions.assertTrue(finalMeet.getTitle().equals("new title"));
     }
 }
