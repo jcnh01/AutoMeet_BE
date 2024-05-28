001218be77d638925317afcd2cd94837788c9255
chanhee01 <joch4267@naver.com>
2024-05-28 21:07:32 +0900

    test : comment 수정 테스트

--- src/test/java/com/AutoMeet/comment/CommentServiceTest.java
+++ src/test/java/com/AutoMeet/comment/CommentServiceTest.java
@@ -68,4 +68,37 @@ public class CommentServiceTest {
         Assertions.assertTrue(commentList.contains("댓글"));
         Assertions.assertTrue(commentList.contains("댓글1"));
     }
+
+    @Test
+    @Transactional
+    @DisplayName("comment 수정")
+    void updateComment() {
+        UserRequestDto userDto = new UserRequestDto("email@naver.com", "password", "kim", 20);
+        userService.join(userDto);
+        User user = userService.findByEmail(userDto.getEmail());
+
+        List<Long> userIds = new ArrayList<>();
+        userIds.add(user.getId());
+
+        UserRequestDto userDto2 = new UserRequestDto("email2@naver.com", "password2", "lee", 30);
+        userService.join(userDto2);
+        User user2 = userService.findByEmail(userDto2.getEmail());
+
+        userIds.add(user2.getId());
+
+        String meetId = meetService.save("title", "content", userIds);
+
+        commentService.createComment(meetId, "댓글", user.getId());
+
+        Meet meeting = meetService.findMeeting(meetId);
+
+        List<Comment> comments = meeting.getComments();
+        String commentId = comments.get(0).getId();
+        // 실제 어플리케이션에서는 list의 commentId를 request로 받아옴
+
+        meeting.updateComment(commentId, "newContent");
+
+        List<String> commentList = comments.stream().map(comment -> comment.getContent()).collect(Collectors.toList());
+        Assertions.assertTrue(commentList.contains("newContent"));
+    }
 }
