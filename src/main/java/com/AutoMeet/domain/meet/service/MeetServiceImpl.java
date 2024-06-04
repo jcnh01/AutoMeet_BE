22c7220a61c64a288567ae7175d2cff50f5bc1d0
chanhee01 <joch4267@naver.com>
2024-06-04 22:51:48 +0900

    fix : flask 서버에 파일 요청 방식 변경

--- src/main/java/com/AutoMeet/domain/meet/service/MeetServiceImpl.java
+++ src/main/java/com/AutoMeet/domain/meet/service/MeetServiceImpl.java
@@ -12,19 +12,29 @@ import com.AutoMeet.domain.meet.model.Meet;
 import com.AutoMeet.domain.meet.repository.MeetRepository;
 import com.AutoMeet.domain.meetingRoom.exception.MeetingNotExistException;
 import com.AutoMeet.domain.user.repository.UserRepository;
+import io.github.classgraph.Resource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -38,15 +48,31 @@ public class MeetServiceImpl implements MeetService {
     private String flask_url = "http://54.82.4.8:5000";
 
     @Override
-    public String textSummarization(String recordingUrl) {
+    public String textSummarization(String recordingUrl) throws IOException  {
         RestTemplate restTemplate = new RestTemplate();
         String url = flask_url + "/api/text_summarization";
 
-        HttpEntity<String> entity = new HttpEntity<>(recordingUrl);
+        // URL에서 파일을 다운로드
+        Path tempFile = Files.createTempFile("recording", ".mp4");
+        try (InputStream in = new URL(recordingUrl).openStream()) {
+            Files.copy(in, tempFile, StandardCopyOption.REPLACE_EXISTING);
+        }
 
-        // flask 서버에 POST 요청 보내기
-        // flask 서버에서 stt로 변환한 다음에 summarization 모델을 통한 요약본을 보내줌
+        // 파일을 MultiValueMap으로 준비
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        FileSystemResource fileResource = new FileSystemResource(tempFile.toFile());
+        body.add("file", fileResource);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        // Flask 서버에 POST 요청 보내기
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+        // 임시 파일 삭제
+        Files.delete(tempFile);
 
         return response.getBody();
     }
@@ -56,11 +82,17 @@ public class MeetServiceImpl implements MeetService {
         RestTemplate restTemplate = new RestTemplate();
         String url = flask_url + "/api/audio_analysis";
 
-        HttpEntity<MultipartFile> entity = new HttpEntity<>(file);
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", file.getResource());
 
-        ResponseEntity<Long> response = restTemplate.exchange(url, HttpMethod.POST, entity, Long.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-        return response.getBody();
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<Map<String, Long>> response = restTemplate.exchange(url, HttpMethod.POST, entity, new ParameterizedTypeReference<Map<String, Long>>() {});
+
+        return response.getBody().get("score");
     }
 
     @Override
@@ -83,7 +115,13 @@ public class MeetServiceImpl implements MeetService {
         RestTemplate restTemplate = new RestTemplate();
         String url = flask_url + "/api/video_analysis";
 
-        HttpEntity<MultipartFile> entity = new HttpEntity<>(file);
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", file.getResource());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(body, headers);
 
         ResponseEntity<VideoAnalysisResponse> response = restTemplate.exchange(url, HttpMethod.POST, entity, VideoAnalysisResponse.class);
 
